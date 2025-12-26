585d6a68aa3ceb73e063dd2e6821afab13896a01
maryia.dziarkach <maryia.derkach@yandex.by>
2025-12-26 22:36:33 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Maryia Dziarkach on 26.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
